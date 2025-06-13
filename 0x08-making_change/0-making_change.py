6ced44087b45415cbca955bc699883e6f48e05db
Tom Morara <morara1920@gmail.com>
2025-06-13 04:24:48 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Making change interview challenge."""
+
+
+def makeChange(coins, total):
+    """Solve the making change interview challenge."""
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
